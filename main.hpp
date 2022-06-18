a550fa8d7086116f2553a3d86ed073842338dbd8
arpit091429 <arpitkhandelwal24@gmail.com>
2022-06-18 17:25:19 +0530

    Add files via upload

---
+++ main.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+//#include <stdio.h>
+#include <stdbool.h>
+
+int  getNoOfRValuesInRange(int* array_p, int startRange, int endRange);
+bool checkInRange(int val, int startRange, int endRange);
