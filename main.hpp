6d28c6978c7cdb003a8a4dc5c21678a550e78eb0
arpit091429 <arpitkhandelwal24@gmail.com>
2022-06-18 17:26:47 +0530

    Update and rename main.hpp to chargingRange.hpp

--- main.hpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-//#include <stdio.h>
-#include <stdbool.h>
-
-int  getNoOfRValuesInRange(int* array_p, int startRange, int endRange);
-bool checkInRange(int val, int startRange, int endRange);
