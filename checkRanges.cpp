93a696f2917de3d64c727c906eb9d9de45244283
arpit091429 <arpitkhandelwal24@gmail.com>
2022-08-19 16:21:58 +0530

    Update checkRanges.cpp

--- checkRanges.cpp
+++ checkRanges.cpp
@@ -1,32 +1,49 @@
-#include "chargingRange.hpp"
+#include "Range.h"
+#include <algorithm> 
+#include <iostream>
 
-bool checkInRange(int val, int startRange, int endRange)
+bool isValueContinuous(int currentIndexValue, int followingIndexValue)
 {
-    if ((val >= startRange) && (val <= endRange))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+   if(((followingIndexValue - currentIndexValue) == 1) || ((followingIndexValue - currentIndexValue) == 0))//Continuous element or same element
+   {
+	   return true;
+   }
+   return false;
 }
 
-int getNoOfRValuesInRange(int* array_p, int startRange, int endRange)
+void checkForContinuityInPeriodicCurrent(std::vector<int> sortedPeriodicCurrents, int CurrentIndexInPeriodicCurrentVector, ContinuityInfo& continuityInfo)
 {
-    int arrayLength  = 0;
-    int valueInRange = 0;
-    arrayLength      = sizeof(array_p);
-    for (int i = 0; i < arrayLength; i++)
-    {
-        if (true == checkInRange(array_p[i], startRange, endRange))
-        {
-            valueInRange++;
-        }
-        else
-        {
-        }
-    }
-    cout<< startRange <<endRange << valueInRange <<endl;
-    return valueInRange;
+	int startIndex = sortedPeriodicCurrents[CurrentIndexInPeriodicCurrentVector];
+   for(int index = CurrentIndexInPeriodicCurrentVector; index < (sortedPeriodicCurrents.size() - 1); index++)
+   {
+	   if(isValueContinuous(sortedPeriodicCurrents[index], sortedPeriodicCurrents[index + 1]))
+	   {
+		   continuityInfo.m_totalReadingContinuousRange ++;
+		   continuityInfo.m_startIndexValueOfContinuousRange = startIndex;
+		   continuityInfo.m_endIndexValueOfContinuousRange = sortedPeriodicCurrents[index + 1];
+	   }
+	   else
+	   {
+		   break;
+	   }
+   }
+}
+
+std::vector<ContinuityInfo> CalculateRangeAndReadings(std::vector<int> periodicCurrentVector)
+{
+    int CurrentIndexInPeriodicCurrentVector = 0;
+	std::vector<ContinuityInfo> continuityInfoList;
+	std::sort(periodicCurrentVector.begin(), periodicCurrentVector.end());
+    while(true == (CurrentIndexInPeriodicCurrentVector < periodicCurrentVector.size()))
+	{
+	   ContinuityInfo continuityInfo;
+           checkForContinuityInPeriodicCurrent(periodicCurrentVector, CurrentIndexInPeriodicCurrentVector, continuityInfo);
+	   if(continuityInfo.m_totalReadingContinuousRange > 1)
+	   {
+		  continuityInfoList.push_back(continuityInfo);
+	      CurrentIndexInPeriodicCurrentVector = CurrentIndexInPeriodicCurrentVector + continuityInfo.m_totalReadingContinuousRange - 1;
+	   }
+	   CurrentIndexInPeriodicCurrentVector++;
+	}
+	return continuityInfoList;
 }
